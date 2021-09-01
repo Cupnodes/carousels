5f59da8a712f062f23bc55b499ca4d0b6e9fa702
cupnodes <aldairgomesdecastrosales@gmail.com>
2021-09-01 10:42:29 -0300

    Primeiro Caroussel

--- carousel_1/script.js
+++ carousel_1/script.js
@@ -1,1 +1,10 @@
-console.log('teste')
+const items = document.querySelector('.items');
+
+items.addEventListener('wheel', e => {
+    if (e.deltaY < 0) {
+        e.target.scrollBy(800, 0);
+    } else {
+        e.target.scrollBy(-800, 0);
+    }
+});
+
